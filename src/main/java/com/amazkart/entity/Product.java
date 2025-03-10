277b3faf6e8d19a20fe34c165b22a01b1b4d6078
safdar.waqar1 <safdar.waqar1@gmail.com>
2025-03-11 03:51:02 +0530

    product and catagory entity created

---
+++ src/main/java/com/amazkart/entity/Product.java
@@ -0,0 +1,59 @@
+package com.amazkart.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "products")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+	private String description;
+	private Double price;
+	private Integer stock;
+
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	@ManyToMany(mappedBy = "products")
+	private List<Cart> carts;
+
+	@ManyToMany(mappedBy = "products")
+	private List<Order> orders;
+
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	private LocalDateTime updatedOn;
+
+	// Getters and Setters
+}
