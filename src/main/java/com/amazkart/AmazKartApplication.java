37e5bda0d4469e8f3b05c0e92aedfdc1a561206f
safdarwaqar <safdar.waqar1@gmail.com>
2025-03-21 21:14:23 +0530

    this is used to test the Git GUI

--- src/main/java/com/amazkart/AmazKartApplication.java
+++ src/main/java/com/amazkart/AmazKartApplication.java
@@ -5,11 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class AmazKartApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AmazKartApplication.class, args);
+		log.info("AmazKart has started successfully...");
+		
 	}
 	
 	@Bean
